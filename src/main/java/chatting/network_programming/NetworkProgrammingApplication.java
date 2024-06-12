d8c140d089feadd5c29e85dcf3e5286e03b774a4
밍소쿠리 <115446238+min-soku@users.noreply.github.com>
2024-06-13 00:33:11 +0900

    0613 commit

---
+++ src/main/java/chatting/network_programming/NetworkProgrammingApplication.java
@@ -0,0 +1,13 @@
+package chatting.network_programming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chatting/network_programming/NetworkProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package chatting.network_programming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
